2bb16d293e94e3bb291cc8a0be6a4928176307d1
vivekdarji00 <70027857+vivekdarji00@users.noreply.github.com>
2022-04-24 11:03:23 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -78,4 +78,4 @@ class TelegraphHelper:
         return
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('ToonsWood.In', 'https://www.toonswood.in')
